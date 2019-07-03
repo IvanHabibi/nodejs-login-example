ba9cb912779fe1cf88b9c7a83b4c9c171b6c241f
Ivan Habibi <ivanhabi2@gmail.com>
2019-07-03 09:20:03 +0700

    add login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Halo Gais!' });
+  // res.render('index', { title: 'Halo Gais!' });
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  // res.render('index', { title: 'Halo Gais!' });
+  console.log(req.body)
+  if(req.body.email=='admin@example.com'&&req.body.pass=='password'){
+    res.render('correct');
+  }else{
+    res.render('wrong');
+  }
+
 });
 
 module.exports = router;
